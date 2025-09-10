09a28a75a09d36eb012efa2f79c485fb138ece8f
kadour <kadourzaroui399@gmail.com>
2025-09-10 07:23:44 -0400

    create Profile models && serializers

---
+++ Profile/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio  = models.TextField(blank=True)
+    profile_image = models.ImageField(upload_to='profile_images/', blank=True, null=True)
+    profile_cover = models.ImageField(upload_to='profile_covers/', blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
---
+++ Profile/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Profile
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'first_name', 'last_name']
+
+class ProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Profile
+        fields = ['user', 'bio', 'profile_image', 'profile_cover']
