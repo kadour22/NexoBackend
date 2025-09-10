db8b852dca9b8f502db328098923a9cd9912aa49
kadour <kadourzaroui399@gmail.com>
2025-09-10 08:32:59 -0400

    install && configure cors-headers

--- Nexo_Backend/settings.py
+++ Nexo_Backend/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'User',
     'Profile',
     'rest_framework',   
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
+    "django.middleware.common.CommonMiddleware",
 ]
 
 ROOT_URLCONF = 'Nexo_Backend.urls'
@@ -176,3 +179,7 @@ REST_FRAMEWORK = {
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     )
 }
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173"
+]
