ee6efcb091092a9cb36813ddfe187653b7aece0b
kadour <kadourzaroui399@gmail.com>
2025-09-10 07:53:01 -0400

    add signals to create user profile automatically ✅

---
+++ User/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from Profile.models import Profile
+from django.contrib.auth.models import User
+
+
+@receiver(post_save,sender=User)
+def create_user_profile_signal(sender, instance, created, **kwargs) :
+    if created :
+        profile = Profile.objects.create(user=instance)
+        profile.save()
+        print("Profile Created")
