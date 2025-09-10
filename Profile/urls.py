abc0fd7751cdce1dfee84088aa150551d14678db
kadour <kadourzaroui399@gmail.com>
2025-09-10 07:36:39 -0400

    create profile endpoint && update profile data endpoint

--- Profile/urls.py
+++ Profile/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = []
+urlpatterns = [
+    path('profile/', views.UserProfile.as_view(), name='user-profile'),
+    path('update-profile/', views.UpdateProfileData.as_view(), name='update-profile'),
+]
--- Profile/views.py
+++ Profile/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status , generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from .serializers import ProfileSerializer
+from .models import Profile
+
+class UserProfile(APIView):
+    permission_classes = [IsAuthenticated]
+    def get(self, request):
+        try:
+            profile = Profile.objects.get(user=request.user)
+            serializer = ProfileSerializer(profile)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except Profile.DoesNotExist:
+            return Response({"detail": "Profile not found."}, status=status.HTTP_404_NOT_FOUND)
+
+class UpdateProfileData(generics.UpdateAPIView):
+    permission_classes = [IsAuthenticated]
+    serializer_class = ProfileSerializer
+
+    def get_object(self):
+        return Profile.objects.get(user=self.request.user)
