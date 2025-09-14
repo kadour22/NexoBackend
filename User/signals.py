4c392a60c6743f549224fdfe3e9d8e893a436ed6
kadour <kadourzaroui399@gmail.com>
2025-09-14 12:57:23 -0400

    fixe Profile signals(2)

--- User/signals.py
+++ User/signals.py
@@ -4,10 +4,9 @@ from django.dispatch import receiver
 from Profile.models import Profile
 from django.contrib.auth.models import User
 
-
 @receiver(post_save,sender=User)
 def create_user_profile_signal(sender, instance, created, **kwargs) :
     if created :
-        profile = Profile.objects.create(user=instance,first_name=instance.first_name,last_name=instance.last_name)
+        profile = Profile.objects.create(user=instance)
         profile.save()
         print("Profile Created")
