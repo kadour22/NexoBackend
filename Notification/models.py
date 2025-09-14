db9f228b4dc64b15528ea51cd1c9d6127e617cd8
kadour <kadourzaroui399@gmail.com>
2025-09-14 08:45:17 -0400

    add migrations

--- Notification/models.py
+++ Notification/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 class Notification(models.Model) :
     message = models.TextField()
     timestamp = models.DateTimeField(auto_now_add=True)
-    mark_as_read =  models.BooleanField(default=False)
+    mark_as_read = models.BooleanField(default=False)
 
     def __str__(self) :
         return f"Notification for {self.message} at {self.timestamp}"
