3c29131e607b4eec88be04a50fd523ea22c36fcb
kadour <kadourzaroui399@gmail.com>
2025-09-15 16:19:42 -0400

    update create comment endpoint

--- Comment/urls.py
+++ Comment/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path("comments-list/<int:post_id>/", views.PostComments.as_view()),
-    path("comments/create/", views.CreateComment.as_view()),
+    path("comments/create/<int:post_id>/", views.CreateComment.as_view()),
 ]
--- Comment/views.py
+++ Comment/views.py
@@ -6,6 +6,7 @@ from .serializers import ListCommentSerializer , CreateCommentSerializer
 from .models import Comment
 from Post.models import Post
 
+from django.shortcuts import get_object_or_404
 
 class PostComments(APIView):
     def get(self,request,post_id) :
@@ -20,9 +21,12 @@ class PostComments(APIView):
             }, status=status.HTTP_200_OK
         )
 
-class CreateComment(generics.CreateAPIView) :
-    serializer_class = CreateCommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-    def perform_create(self, serializer) :
-        return serializer.save(user=self.request.user)
+class CreateComment(APIView) :
+    def post(self, request, post_id) :
+        serializer = CreateCommentSerializer(data=request.data)
+        post = get_object_or_404(Post, id=post_id)
+        if serializer.is_valid() :
+            serializer.save(user=request.user, post=post)
+            return Response(serializer.data, status=200)
+        else :
+            return Response(serializer.errors , status=400)
